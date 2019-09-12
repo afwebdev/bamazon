2c7664edce8a471408c72358f00306507652792c
AFWebDev <andrewfariaj@gmail.com>
2019-09-12 15:34:34 -0400

    update seeds

--- seeds.sql
+++ seeds.sql
@@ -1,20 +1,16 @@
 INSERT INTO inventory(item_name, item_price, quantity)
 VALUES('FlipFlops - BLUE', 12.99, 43)
-
 INSERT INTO inventory(item_name, item_price, quantity, department)
 VALUES('Magic Mirror', 39.99, 5, 4 )
-
 INSERT INTO inventory(item_name, item_price, quantity, department)
 VALUES('Car Battery', 79.99, 20, 1 )
-
 INSERT INTO inventory(item_name, item_price, quantity, department)
 VALUES('FlipFlops - BLACK', 12.99, 10, 8 )
-
 INSERT INTO inventory(item_name, item_price, quantity, department)
 VALUES('PlayStation 4', 499.99, 30, 2 )
-
 INSERT INTO inventory(item_name, item_price, quantity, department)
 VALUES('Bananas', 1.90, 10, 5 )
 
 
---- + More
+INSERT into sales(item_id, total_sales, department)
+VALUES(5, 1009.94, 3 )
